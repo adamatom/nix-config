881b6350778b79020121c471a631a6560a9ed14c
Adam Labbe <adam@hfi.io>
2025-07-06 13:47:08 +0200

    add mcfly

--- home/adam.nix
+++ home/adam.nix
@@ -12,7 +12,7 @@
 
     # CLI tools
     vimHugeX wget git killall zip xz unzip ripgrep jq dnsutils nmap file which tree
-    gnused gnutar gawk zstd gnupg bat neofetch picocom zoxide wl-clipboard
+    gnused gnutar gawk zstd gnupg bat neofetch picocom zoxide wl-clipboard fzf mcfly
 
     # Development tools
     pkg-config openssl gcc clang-tools clang-analyzer

bf5c4e874a8811d3bd7d61d3e020a1362b5ebb34
Adam Labbe <adam@hfi.io>
2026-02-15 15:44:13 +0100

    context-home: always force x11 platform for minecraft

--- home/adam/context-home.nix
+++ home/adam/context-home.nix
@@ -4,8 +4,11 @@
   home.packages = lib.mkAfter [
     (pkgs.writeShellScriptBin "prismlauncher-env" ''
       exec env \
+        GLFW_PLATFORM="x11" \
+        LIBGL_DRIVERS_PATH="/run/opengl-driver/lib/dri" \
         LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib" \
         __GLX_VENDOR_LIBRARY_NAME="nvidia" \
+        __EGL_VENDOR_LIBRARY_DIRS="/run/opengl-driver/share/glvnd/egl_vendor.d" \
         __EGL_VENDOR_LIBRARY_FILENAMES="/run/opengl-driver/share/glvnd/egl_vendor.d/50_nvidia.json" \
         ${pkgs.prismlauncher}/bin/prismlauncher "$@"
     '')
